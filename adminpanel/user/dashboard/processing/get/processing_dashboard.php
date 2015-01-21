0e4f4e15e485ff368aa365494125b0b9808dfe1e
Soellner Patrick <patrick.soellner@publicis.de>
2015-01-21 15:34:27 +0100

    Change dashboard with bootstrap

--- adminpanel/user/dashboard/processing/get/processing_dashboard.php
+++ adminpanel/user/dashboard/processing/get/processing_dashboard.php
@@ -2,7 +2,12 @@
 
 use Symfony\Component\HttpFoundation\Response;
 
+$loggedInSince = $app['session']->get( 'time' );
+
+$timeOfLogin = date( 'r', $loggedInSince );
+
 $render = $app['twig']->render( 'dashboard.twig', array(
+    'loggedinsince' => $timeOfLogin,
     'is_active_dashboard' => true
 ) );
 
