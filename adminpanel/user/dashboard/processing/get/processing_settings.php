67fcd14754ae789c971b2569f256dc04432ef536
Soellner Patrick <patrick.soellner@publicis.de>
2015-01-21 15:45:17 +0100

    Change settings with bootstrap

--- adminpanel/user/dashboard/processing/get/processing_settings.php
+++ adminpanel/user/dashboard/processing/get/processing_settings.php
@@ -2,8 +2,6 @@
 
 use Symfony\Component\HttpFoundation\Response;
 
-$render = $app['twig']->render( 'settings.twig', array(
-    'headline' => 'Wilkommen auf ihrem Profil'
-        ) );
+$render = $app['twig']->render( 'settings.twig' );
 
 return new Response( $render, 201 );
