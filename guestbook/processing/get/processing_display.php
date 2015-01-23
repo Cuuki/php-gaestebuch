e1191d4c069096e548502e16ab40dcc8de1ca295
Soellner Patrick <patrick.soellner@publicis.de>
2015-01-23 11:05:29 +0100

    Add functionality for showing header and sidebar in guestbook if user is logged in

--- guestbook/processing/get/processing_display.php
+++ guestbook/processing/get/processing_display.php
@@ -4,9 +4,17 @@ use Symfony\Component\HttpFoundation\Response;
 
 $posts = getPosts( $app['db'], $rowsperpage, $currentpage );
 
+$isLogged = false;
+
+if ( $app['session']->get( 'user' ) )
+{
+    $isLogged = true;
+}
+
 $render = $app['twig']->render( 'guestbook.twig', array(
     'headline' => 'Tragen Sie sich ein:',
     'submit_text' => 'Hinzufügen',
+    'is_logged_in' => $isLogged,
     'posts' => $posts,
     'firstpage' => $firstPage,
     'currentpage' => $currentpage,
