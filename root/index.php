1f14984fd7ccf11c490861e62ae868dcfb10e438
Patrick Söllner <patrick.soellner@publicis.de>
2015-02-06 12:25:21 +0100

    Move templates out of root directory Change path in index.php for templates

--- root/index.php
+++ root/index.php
@@ -13,7 +13,7 @@ $app = new Silex\Application();
 $app->register( new Silex\Provider\UrlGeneratorServiceProvider() );
 $app->register( new Silex\Provider\SessionServiceProvider() );
 $app->register( new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__ . '/templates'
+    'twig.path' => __DIR__ . '/../templates'
 ) );
 
 //Datenbankverbindung registrieren
